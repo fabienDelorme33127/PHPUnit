8475d6480eaae53158e1d3b1830145df4dfac80d
delorme <delormefabien33@gmail.com>
2022-05-31 18:40:35 +0200

    first commit, start phpunit udemy

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{   
+    public function testAddition()
+    {
+        $this->assertEquals(4, 2+2);
+    }
+
+}
