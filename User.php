6414a185f23f77927c4521c63bb0d73e49edab77
fabienDelorme33127 <delormefabien33@gmail.com>
2022-06-07 15:33:31 +0200

    dependency injection

--- User.php
+++ User.php
@@ -4,9 +4,26 @@ class User
 {
         public $fistName;
         public $surname;
+        public $email;
+
+        /**
+         * Mailer object
+         * @var Mailer
+         */
+        protected $mailer;
+
+        public function setMailer(Mailer $mailer)
+        {
+                $this->mailer = $mailer;
+        }
 
         public function getFullName()
         {
                 return trim($this->fistName . ' ' . $this->surname);
+        }
+
+        public function notify($message)
+        {
+                return $this->mailer->sendMessage($this->email, $message);
         }
 }
--- tests/MockTest.php
+++ tests/MockTest.php
@@ -2,10 +2,12 @@
 
 namespace App\Tests;
 
+use User;
 use Mailer;
 use PHPUnit\Framework\TestCase;
 
 require_once 'Mailer.php';
+require_once 'User.php';
 
 class Mocktest extends TestCase
 { 
@@ -16,8 +18,26 @@ class Mocktest extends TestCase
                 $mock->method('sendMessage')
                         ->willReturn(true);
 
-                $result = $mock->sendMessage('bob@gmail.com', 'hello wolrd');
+                $result = $mock->sendMessage('bob@gmail.com', 'hello world');
 
                 $this->assertTrue($result);
+        }
+
+        public function testNotifyIsSent()
+        {
+                $user = new User();
+
+                $mock_mailer = $this->createMock(Mailer::class);
+
+                $mock_mailer->expects($this->once())
+                                ->method('sendMessage')
+                                ->with($this->equalTo('testouille@gmail.com'), $this->equalTo('msg sent'))
+                                ->willReturn(true);
+
+                $user->setMailer($mock_mailer);              
+
+                $user->email = 'testouille@gmail.com';
+
+                $this->assertTrue($user->notify('msg sent'));
         }
 }
